a55f86b8c246cca14e18c6e694ad3ae7b4606249
Hakobyan001 <vahagn.hakobyan.2001@gmail.com>
2023-03-02 13:48:17 +0400

    with 0-s

--- src/middlewares/validation/schemes/faction.shcema.js
+++ src/middlewares/validation/schemes/faction.shcema.js
@@ -28,8 +28,8 @@ const FactionSchema = {
     }),
     body: Joi.object({
       name: Joi.string().min(3).max(9555),
-      member1: Joi.string().max(555),
-      member2: Joi.string().max(555),
+      member1: Joi.string().min(0).max(555),
+      member2: Joi.string().min(0).max(555),
       cityphone: Joi.string().min(0),
       internalphone: Joi.string().min(0)
     }).or(
